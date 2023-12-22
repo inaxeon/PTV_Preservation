080bff0bbb0f6964c3db5b105e1c263ea07c740a
Matthew Millman <Matt@inaxeon.local>
2023-12-22 10:50:48 +0000

    Tidy up PALs. Added P22V10 builds

--- PM5644/PM8546/OpenPM8546/V28_MONO/BUILD.bat
+++ PM5644/PM8546/OpenPM8546/V28_MONO/BUILD.bat
@@ -1,1 +1,12 @@
-c:\Wincupl\shared\cupl -m1lxfjnae -u C:\Wincupl\Shared\cupl.dl V28.PLD
+@ECHO OFF
+c:\Wincupl\shared\cupl -m1lxfjae -u C:\Wincupl\Shared\cupl.dl g22v10 V28.PLD
+DEL V28_G22V10.JED
+REN V28.JED V28_G22V10.JED
+c:\Wincupl\shared\cupl -m1lxfjae -u C:\Wincupl\Shared\cupl.dl p22v10 V28.PLD
+DEL V28_P22V10.JED
+REN V28.JED V28_P22V10.JED
+DEL *.mx
+DEL *.sim
+DEL *.lst
+DEL *.abs
+DEL *.doc
